92f78964817cda8bc837c12dbba1870631b8f492
Hari Narayanan <harinarayanan4057@gmail.com>
2024-06-05 21:34:07 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RootLayout = ({
+    children
+}:{children:React.ReactNode}) =>{
+return (
+        <html lang="en">
+                <body>{children}</body>
+        </html>
+)
+}
+
+
+export default RootLayout;
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Page = () =>{
+    return <h1>Hi this is a test site</h1>
+}
+
+
+export default Page;
